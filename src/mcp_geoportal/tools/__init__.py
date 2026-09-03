5ac88c64b49ec8f765114044e7b3f7dc20d01614
Peter Schär <peter.schaer@be.ch>
2026-09-03 11:02:17 +0200

    Version erhöht auf v0.2.0

--- src/mcp_geoportal/tools/__init__.py
+++ src/mcp_geoportal/tools/__init__.py
@@ -1,8 +1,3 @@
-# from . import base_tools
-# from . import create_map_link
-# from . import oereb_tools
-# from . import gp_tools
-
 from .oereb_tools import __get_oereb_auszug, __get_oereb_themes
 from .base_tools import __get_bfsnr_for_gemeinde, __get_egrid_from_address
 from .gp_tools import __get_bohrprofile_for_egrid, __get_gemeinde_infos, __get_naturgefahren_for_egrid, __get_property_info_for_egrid
